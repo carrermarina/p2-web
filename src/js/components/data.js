65c1212ce8655f07ca335c7461eb58d3e6d34985
marin <marina.o12@hotmail.es>
2021-12-18 00:19:55 +0100

    canvis

--- src/js/components/data.js
+++ src/js/components/data.js
@@ -1,4 +1,3 @@
-HEAD
 class Character {
     constructor(key, name, role, portrait, message) {
       this.key = key;
@@ -9,18 +8,4 @@ class Character {
     }
   }
 
-
-Vue.component('poke-button', {
-    props: {
-        data: Object
-    },
-    template: '<button v-on:click="loadPokeData">{{ data.name }}</button>',
-    methods: {
-        loadPokeData() {
-            fetch('http://localhost:<3000>/api/detail' + this.data.id)
-                .then(response => response.json())
-                .then(data => this.$parent.pokedata = data.abilities[0].ability.name);
-        }
-    }
-})
 
