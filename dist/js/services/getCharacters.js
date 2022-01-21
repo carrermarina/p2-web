954c6c750e99f94ea51240c0b87c36f73d48e61e
martared2001 <mrcasas2001@gmail.com>
2022-01-21 16:16:16 +0100

    solucionat fulldata

--- dist/js/services/getCharacters.js
+++ dist/js/services/getCharacters.js
@@ -2,15 +2,16 @@ export default class GetCharactersService {
 
     constructor () { this.fulldata = [] }
 
-    execute(searchInput) {
+    async execute (searchInput) {
         
         console.log(searchInput)
-        fetch (
+        const response = await fetch (
             'http://localhost:3000/api/list?search=' + searchInput
         )
         .then(response => response.json())
-        .then(data => /*this.fulldata = data*/ console.log(data));
-
+        .then(data => this.fulldata = data)
+         //aixo no es passa correctament, data s'omple pero fulldata no
+        console.log(this.fulldata)
         return this.fulldata
     }
 }
