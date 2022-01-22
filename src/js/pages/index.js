98ddcbfe1b029e481d7d81a6321cbcde91a81cac
martared2001 <mrcasas2001@gmail.com>
2022-01-22 13:35:15 +0100

    ben fet

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,7 +1,6 @@
 import {} from "../components/button.js";
 import {} from "../components/fichas.js";
 import {} from "../components/searchbar.js";
-import GetCharacters from "../services/getCharacters.js";
 
 const app = new Vue({
   el: "#Personajes",
