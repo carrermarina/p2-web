e4e04a01a9a50849a7f73f5a6723c4ce102e7ea8
carrermarina <marina.o12@hotmail.es>
2022-01-23 16:59:05 +0100

    escrit getcharaccter el dani amo

--- src/js/services/getCharacters.js
+++ src/js/services/getCharacters.js
@@ -16,4 +16,11 @@ export default class GetCharactersService {
     }
 }
 
+/*El fetch s'executa en assíncron, per tant, la línia "return this.fulldata" s'executa abans de que el fetch resolgui. 
+Tal com ho teniu us aconsello que feu servir await del fetch d'aquesta manera:
+
+const response = await fetch(...).then(...).then(data=>data);
+this.fulldata = response.text() // o response.JSON() el que necessiteu
+
+D'aquesta manera s'espera a que el fetch acabi i es guarda el resultat a fulldata. */
 
